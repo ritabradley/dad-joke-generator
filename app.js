152f495194c5b3b0a800b43778843f3d57a98536
Rita Bradley <rita@ritabradley.dev>
2022-10-26 23:19:51 -0700

    create function to fetch new joke

---
+++ app.js
@@ -0,0 +1,11 @@
+const joke = document.querySelector('#joke');
+const generateBtn = document.querySelector('#generate-joke');
+const url = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = async () => {
+    const res = await fetch(url, {
+        headers: { accept: 'application/json' },
+    });
+    const jokeData = await res.json();
+    joke.innerText = jokeData.joke;
+};
