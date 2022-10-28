fdd9c257269bfc09802414dada710d3f50431560
Rita Bradley <rita@ritabradley.dev>
2022-10-27 18:28:57 -0700

    refactor DOM manipulation to its own function

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const joke = document.querySelector('#joke');
 const generateBtn = document.querySelector('#generate-joke');
 const url = 'https://icanhazdadjoke.com/';
 
+const addJokeContent = async () => {
+    const jokeContent = await fetchJoke()
+
+    try {
+        joke.innerText = jokeContent;
+    } catch (e) {
+        console.log("Something went wrong!",e);
+    }
+}
+
 const fetchJoke = async () => {
     try {
         const res = await fetch(url, {
             headers: { accept: 'application/json' },
         });
         const jokeData = await res.json();
-        joke.innerText = jokeData.joke;
+        return jokeData.joke;
     } catch (err) {
         console.error("Oh No! Something went wrong.", err);
     }
 };
 
-generateBtn.addEventListener('click', fetchJoke);
+generateBtn.addEventListener('click', addJokeContent);
